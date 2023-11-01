d3040e976181954b56cdff9776c891e48d0a21ea
Jorge <jorge.guerrero@foundever.com>
2023-11-01 08:11:17 -0500

    Add project files.

---
+++ Algorithms/MultiplesOf3and5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerAlgorithms.Algorithms
+{
+    public class MultiplesOf3and5
+    {
+        public int multiplesOf3and5(int number)
+        {
+            int sum = 0;
+            int m3 = 0;
+            int m5 = 0;
+
+            for (int i = 0; i < number; i++)
+            {
+                m3 = i % 3;
+                m5 = i % 5;
+
+                if (m3 == 0 || m5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using ProjectEulerAlgorithms.Algorithms;
+
+//Function that returns the sum of the multiples of 3 or 5. 
+MultiplesOf3and5 sampleOne = new MultiplesOf3and5();
+int result = sampleOne.multiplesOf3and5(49);
+Console.WriteLine($"The sum of the multiples is: {result}");
