4e985ff5e23b5e76c121d27c34b6e21b49f01cf4
Jorge <jorge.guerrero@foundever.com>
2023-11-02 11:31:53 -0500

    Smallest Multiple Algorithm

---
+++ Algorithms/SmallestMultiple.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerAlgorithms.Algorithms
+{
+    public class SmallestMultiple
+    {
+       
+        public long smallestMultiple(long num) 
+        {
+            int sn = 0;
+            bool found = false;
+            while (!found)
+            {
+                sn += 1;
+                bool isDivisible = true;
+                for (int i = 1; i <= num; i++)
+                {
+                    if ((sn % i) != 0)
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+                if(isDivisible) found = true;
+            }
+            return sn;
+        }
+    }
+}
