d091e6756791042c42d55da93cb94d5af9c1325f
Jose Miguel Cardenas Serpa <67446050+Jcardenas99S@users.noreply.github.com>
2023-11-11 18:40:52 -0500

    Update MultiplesOf3and5.cs

--- Algorithms/MultiplesOf3and5.cs
+++ Algorithms/MultiplesOf3and5.cs
@@ -8,20 +8,25 @@ namespace ProjectEulerAlgorithms.Algorithms
 {
     public class MultiplesOf3and5
     {
+        /* 
+            Find the sum of all the multiples of 3 or 5 below the provided parameter value number. 
+        */
         public int multiplesOf3and5(int number)
         {
             int sum = 0;
 
             for (int i = 0; i < number; i++)
             {
+                
                int m3 = i % 3;
                int m5 = i % 5;
-
+                //Here we check if the numer is multpile of 3 or 5 and if its correct we add that number.
                 if (m3 == 0 || m5 == 0)
                 {
                     sum += i;
                 }
             }
+            
             return sum;
         }
     }
