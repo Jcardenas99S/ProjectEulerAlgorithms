7e58198e41c0d30d6a844ae52b16967e8747547b
Jose <jorge.guerrero@foundever.com>
2023-12-21 11:28:06 -0500

    10001

--- Algorithms/10001stPrime.cs
+++ Algorithms/10001stPrime.cs
@@ -6,5 +6,40 @@ using System.Threading.Tasks;
 
 namespace ProjectEulerAlgorithms.Algorithms
 {
-    
+    public class _10001stPrime
+    {
+        /*
+            What is the nth prime number?
+         */
+        public long nthPrime(long num)
+        {
+            long n = 2;
+            long count = 0;
+            long lastPrime = 0;
+            bool found = false;
+            while (!found)
+            {
+                bool isPrime = true;
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    count++;
+                    if (count == num)
+                    {
+                        lastPrime = n;
+                        found = true;
+                    }
+                }
+                n++;
+            }
+            return lastPrime;
+        }
+    }
 }
